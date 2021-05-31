d3b0414ac04709c4de5a330bb4423f0835b37474
Andrii86 <Andrii86@gmail.com>
2021-05-31 17:33:43 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-import {createStore} from './createStore'
-import './style.css'
+import {render} from "redux-logger/src/diff";
+import thunk from "redux-thunk";
+import {rootReducer} from "./redux/rootReducer";
+import {applyMiddleware, createStore} from 'redux';
+import './style.css';
+import {asyncIncrement, decrement, increment} from "./redux/action";
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
 const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 
-const store = createStore()
+const store = createStore(
+    rootReducer,
+    0,
+    applyMiddleware(thunk)
+)
 
 addBtn.addEventListener('click', () => {
-
+    store.dispatch(increment());
 })
-subBtn.addEventListener('click', () => {
 
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement())
 })
 
 asyncBtn.addEventListener('click', () => {
-
+    store.dispatch(asyncIncrement())
 })
+
+store.subscribe(() => {
+    const state = store.getState()
+    counter.textContent = state
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
